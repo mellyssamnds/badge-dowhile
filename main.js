ef76364282e90e1ba8fa640e1b61d505a0067053
mellyssastephanny <mellyssah.mendes@live.com>
2021-10-24 09:56:34 -0300

    A04 - add main.js e estilizando badge

---
+++ main.js
@@ -0,0 +1,32 @@
+function showMeSomething() {
+  alert(links.github)
+}
+
+const SocialMediaLinks = {
+  linkedin: 'mellyssas',
+  github: 'mellyssastephanny',
+  twitter: 'mlyssa_'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${SocialMediaLinks[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfo() {
+  const url = `https://api.github.com/users/${SocialMediaLinks.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      username.textContent = data.name
+      userbio.textContent = data.bio
+      userimage.src = data.avatar_url
+    })
+}
+
+getGithubProfileInfo()
